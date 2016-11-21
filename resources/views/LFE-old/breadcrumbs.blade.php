b28a20220ad2f9b4e72dae7db45665f2139cbe2a
Stanislav Kabin <me@h-zone.ru>
2016-11-21 17:57:11 +0300

    cleanup

--- resources/views/LFE-old/breadcrumbs.blade.php
+++
@@ -1,6 +0,0 @@
-<ol class="breadcrumb">
-	<li><a href="{{ url( config( 'LFE.routes.prefix' ) ) }}">Forums</a></li>
-@if(!empty($Breadcrumbs))
-@endif
-{{-- //todo: сделать рекурсивный проход по id|title всего дерева вверх --}}
-</ol>
