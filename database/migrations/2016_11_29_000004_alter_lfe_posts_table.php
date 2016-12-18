5ffb102e5a4bba1dab9135de3ab77c1f3f21cf17
Stanislav Kabin <me@h-zone.ru>
2016-12-18 12:50:00 +0300

    Column forum_id for posts table to cache information

---
+++ database/migrations/2016_11_29_000004_alter_lfe_posts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLfePostsTable extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('lfe_posts', function (Blueprint $table) {
+			$table->integer('forum_id')->index();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('lfe_posts', function (Blueprint $table) {
+			$table->dropColumn('forum_id');
+		});
+	}
+}
